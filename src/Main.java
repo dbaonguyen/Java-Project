84ad25e600a6a5d659e2390f6409f201a47a74e7
Nguyen Danh Bao <baond39@gmail.com>
2023-08-20 20:28:49 +0700

    Update

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-<<<<<<< HEAD
-
-=======
         System.out.println("Hi");
->>>>>>> f182778b31136b022b013a6217e66c2db541c1a1
+
     }
 }
