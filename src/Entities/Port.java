4f41e78afae96e15750db0932886699946ae77f9
Nguyen Danh Bao <baond39@gmail.com>
2023-08-23 23:10:09 +0700

    Update Port

--- src/Entities/Port.java
+++ src/Entities/Port.java
@@ -15,12 +15,60 @@ public class Port implements IPort {
     private List<Container> containers = new ArrayList<>();
     private List<Vehicle> vehicles = new ArrayList<>();
 
-    public Port(int portID, String name, double latitude, double longtitude, int capacity, boolean landingAbility) {
+    public Port(String portID, String name, double latitude, double longtitude, int capacity, boolean landingAbility) {
         this.portID = portID;
         this.name = name;
         this.latitude = latitude;
         this.longtitude = longtitude;
         this.capacity = capacity;
+        this.landingAbility = landingAbility;
+    }
+
+    public String getPortID() {
+        return portID;
+    }
+
+    public void setPortID(String portID) {
+        this.portID = portID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongtitude() {
+        return longtitude;
+    }
+
+    public void setLongtitude(double longtitude) {
+        this.longtitude = longtitude;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean isLandingAbility() {
+        return landingAbility;
+    }
+
+    public void setLandingAbility(boolean landingAbility) {
         this.landingAbility = landingAbility;
     }
 
