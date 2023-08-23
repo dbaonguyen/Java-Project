a841fa6c2e5eee847f61caf04a721530fe875ddb
Nguyen Danh Bao <baond39@gmail.com>
2023-08-23 21:52:26 +0700

    Update Port Interface

--- src/Entities/Port.java
+++ src/Entities/Port.java
@@ -1,4 +1,43 @@
 package Entities;
 
-public class Port {
+import Interface.IPort;
+
+import java.awt.*;
+
+public class Port implements IPort {
+
+    @Override
+    public void addContainer(Container container) {
+
+    }
+
+    @Override
+    public void removeContainer(Container container) {
+
+    }
+
+    @Override
+    public void addVehicle(Vehicle vehicle) {
+
+    }
+
+    @Override
+    public void removeVehicle(Vehicle vehicle) {
+
+    }
+
+    @Override
+    public void searchVehicleById(String id) {
+
+    }
+
+    @Override
+    public void searchVehicleByName(String name) {
+
+    }
+
+    @Override
+    public void addTrip(Trip trip) {
+
+    }
 }
--- src/Interface/IPort.java
+++ src/Interface/IPort.java
@@ -1,4 +1,16 @@
 package Interface;
 
+import Entities.Trip;
+import Entities.Vehicle;
+
+import java.awt.*;
+
 public interface IPort {
+    public void addContainer(Container container);
+    public void removeContainer(Container container);
+    public void addVehicle(Vehicle vehicle);
+    public void removeVehicle(Vehicle vehicle);
+    public void searchVehicleById(String id);
+    public void searchVehicleByName(String name);
+    public void addTrip(Trip trip);
 }
