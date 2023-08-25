3f9c382983768ad02c7c68ad0070ea02769f638b
troll <s3978165@rmit.edu.vn>
2023-08-25 22:56:01 +0700

    change small vehicle

--- src/Entities/Truck.java
+++ src/Entities/Truck.java
@@ -2,7 +2,7 @@ package Entities;
 
 public class Truck extends Vehicle {
 
-    public Truck(int vehicleID, String name, double currentFuel, int capacity, double fuelCapacity, Port port, int totalContainers, int totalContainersByType) {
+    public Truck(String vehicleID, String name, double currentFuel, double capacity, double fuelCapacity, Port port, int totalContainers, int totalContainersByType) {
         super(vehicleID, name, currentFuel, capacity, fuelCapacity, port, totalContainers, totalContainersByType);
     }
 }
