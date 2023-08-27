1f2d6f93ae4e22fc783e7927101e638116b758b4
jack <nguyenhuukhoi2004@gmail.com>
2023-08-27 22:04:52 +0700

    Update truck

--- src/Entities/Ship.java
+++ src/Entities/Ship.java
@@ -4,4 +4,6 @@ public class Ship extends Vehicle{
     public Ship(String vehicleID, String name, double currentWeight, double currentFuel, double capacity, double fuelCapacity, Port port) {
         super(vehicleID, name,currentWeight, currentFuel, capacity, fuelCapacity, port);
     }
+
+
 }
--- src/Entities/Truck.java
+++ src/Entities/Truck.java
@@ -1,8 +1,13 @@
 package Entities;
+import java.util.*;
 
 public class Truck extends Vehicle {
 
-    public Truck(String vehicleID, String name, double currentWeight, double currentFuel, double capacity, double fuelCapacity, Port port) {
+    public Truck(String vehicleID, String name, double currentWeight, double currentFuel, double capacity, double fuelCapacity, Port port, ArrayList<Type> type) {
         super(vehicleID, name,currentWeight, currentFuel, capacity, fuelCapacity, port);
+        this.type = new ArrayList<>();
+        type.add(dryStorage);
+        type.add(openTop);
+        type.add(openSide);
     }
 }
