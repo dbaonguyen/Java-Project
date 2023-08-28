07fd7c6c1bd93d0af1a965d9cd5b320498c7308d
jack <nguyenhuukhoi2004@gmail.com>
2023-08-28 20:50:54 +0700

    Update type; tanker, reefer

--- src/Entities/TankerTruck.java
+++ src/Entities/TankerTruck.java
@@ -5,4 +5,36 @@ public class TankerTruck extends Vehicle implements IVehicle{
     public TankerTruck(String vehicleID, String name, double currentWeight, double currentFuel, double capacity, double fuelCapacity, Port port) {
         super(vehicleID, name,currentWeight, currentFuel, capacity, fuelCapacity, port);
     }
+
+    public void truckLoadContainer(Container container){
+        if(container.getType().getType().equals("Liquid")){
+            super.loadContainer(container);
+            //only if loading is possible, the fuel consumption will be updated
+            if(super.loadContainer(container)){
+                this.setFuelConsumption(getFuelConsumption() + container.getTruckConsumption());
+            }
+        }
+        else{
+            System.out.println("This container type can not be loaded onto this vehicle!");
+        }
+    }
+    //unload method for trucks
+    public void truckUnloadContainer(Container container){
+        super.loadContainer(container);
+        //only if unloading is possible, the fuel consumption will be updated
+        if(super.loadContainer(container)){
+            this.setFuelConsumption(getFuelConsumption() - container.getTruckConsumption());
+        }
+    }
+
+    @Override
+    public boolean canMoveToPort(Port port){
+        //check port condition
+        if (!port.isLandingAbility()) {
+            return false;
+        }
+        else{
+            return super.canMoveToPort(port);
+        }
+    }
 }
