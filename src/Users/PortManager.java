2b92910786d7eee074f26b2143ca9ab70da98d8f
admin <s3978690@rmit.edu.vn>
2023-09-01 01:58:13 +0700

    em ruy

--- src/Users/PortManager.java
+++ src/Users/PortManager.java
@@ -8,11 +8,9 @@ public class PortManager extends User{
 
     public PortManager() {
         userCredentials = new HashMap<>();
-        // Initialize the map with usernames and passwords
         userCredentials.put("admin", "adminPassword");
         userCredentials.put("user1", "password1");
         userCredentials.put("user2", "password2");
-        // Add more username-password pairs as needed
     }
 
     public String getPasswordForUsername(String username) {
