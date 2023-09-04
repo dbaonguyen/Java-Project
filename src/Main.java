941f9a8e623aa2caf497382766aa35b20bf19f7a
troll <s3978165@rmit.edu.vn>
2023-09-04 22:13:52 +0700

    reogarnize code and added interface for port manager

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
-import Entities.*;
-import Users.Admin;
-import Users.PortManager;
-import Users.User;
-import java.io.*;
-import java.sql.SQLOutput;
-import java.util.*;
-
 public class  Main {
     public static void main(String[] args) {
-        EmRuy.emruy();
+        AdminInterface.run();
     }
 }
