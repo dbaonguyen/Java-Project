7409353c362d05b965a7753de63453f713d42703
Nguyen Danh Bao <baond39@gmail.com>
2023-09-05 22:15:30 +0700

    update portmanger

--- src/Users/PortManager.java
+++ src/Users/PortManager.java
@@ -1,12 +1,16 @@
 package Users;
 
+import Entities.Port;
+
 import java.io.Serializable;
 import java.util.Map;
 import java.util.HashMap;
 
 public class PortManager extends User implements Serializable {
-    public PortManager(String username, String password) {
-        super(username, password);
-    }
+    public Port portManaged;
 
+    public PortManager(String username, String password, Port portManaged) {
+        super(username, password);
+        this.portManaged = portManaged;
+    }
 }
